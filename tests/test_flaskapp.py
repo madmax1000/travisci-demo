db8f949a9552a3b2ddb72fa5fd0e6921ff85bf92
Max <maxbijkerk@gmail.com>
2019-04-12 12:24:38 +0200

    asdf

--- tests/test_flaskapp.py
+++ tests/test_flaskapp.py
@@ -20,7 +20,7 @@ class FlaskTestCase(unittest.TestCase):
 
     def test_uppercase(self):
         response = self.app.get('/touppercase?s=abc')
-        resp = response.data
+        resp = str(response.data)
         self.assertEqual(resp, "ABC", 'Uppercase endpoint failed known answer abc -> ABC')
 
 
